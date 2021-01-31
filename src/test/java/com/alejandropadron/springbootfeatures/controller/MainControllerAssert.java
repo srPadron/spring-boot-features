21d6233803193460887e1f691534f5765034e88f
Alajenadro Padrón <apadarm@gmail.com>
2021-01-31 16:13:08 +0100

    add MainController test

---
+++ src/test/java/com/alejandropadron/springbootfeatures/controller/MainControllerAssert.java
@@ -0,0 +1,12 @@
+package com.alejandropadron.springbootfeatures.controller;
+
+import org.assertj.core.api.AbstractAssert;
+
+public class MainControllerAssert extends AbstractAssert<MainControllerAssert, MainController> {
+
+    public MainControllerAssert(MainController mainController, Class<?> selfType) {
+        super(mainController, selfType);
+    }
+
+
+}
